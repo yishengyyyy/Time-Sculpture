59dbb1538e0b99d1f4fd9b7783947988351ffe87
yishengyyyy <113206610+yishengyyyy@users.noreply.github.com>
2022-11-09 09:24:32 -0500

    update

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,6 +3,18 @@
 
 $("body").css("overflow", "hidden");
 
+// $(document).ready(function(){
+//     $(".btn").mouseover(function() {
+//         $(this).find('img').toggle();
+//         $(this).find('span').toggle();
+//       }).mouseout(function() {
+//         $(this).find('img').toggle();
+//         $(this).find('span').toggle();
+
+//       })
+
+//       });
+      
 
 var startDate = new Date();
   var startTime = startDate.getTime();
